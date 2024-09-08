db9f4e09854d1640517aa902cbf298b9bc2e81e1
umutcanbas <umutbas57@gmail.com>
2024-09-08 15:41:36 +0300

    Daily card

---
+++ src/components/DailyCard/DailyCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DailyCard = () => {
+  return (
+    <View>
+      <Text style={{color:'white'}}>DailyCard</Text>
+    </View>
+  )
+}
+
+export default DailyCard
+
+const styles = StyleSheet.create({})

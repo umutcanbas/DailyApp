5aa0c141602255473bb098f16722f71df1762b62
umutcanbas <umutbas57@gmail.com>
2024-09-18 17:40:17 +0300

    Profile page UI

--- src/screens/Home/Profile.js
+++ src/screens/Home/Profile.js
@@ -1,18 +1,25 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
 
 import Button from '../../components/Button/Button';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import auth from '@react-native-firebase/auth';
 
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import routes from '../../navigation/routes';
+import LinearGradient from 'react-native-linear-gradient';
 
+import BackArrow from '../../assets/svg/left-arrow.svg';
 
 const Profile = () => {
-
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   const logOut = async () => {
     try {
@@ -22,19 +29,50 @@ const Profile = () => {
       //flashmessage ekle
       navigation.navigate(routes.AUTH_NAVIGATOR, {
         screen: routes.LOGIN,
-      })
+      });
     } catch (error) {
       console.error('Error removing isLogged:', error);
     }
   };
   return (
-    <SafeAreaView>
-      <Text>Profile</Text>
-      <Button title='Log Out' onPress={logOut} />
-    </SafeAreaView>
-  )
-}
+    <LinearGradient
+      colors={['#101020', '#8a42ec']}
+      style={styles.linearGradient}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => navigation.goBack()}>
+            <BackArrow width={29} height={29} />
+          </TouchableOpacity>
 
-export default Profile
+          <Text style={styles.headerText}>Profile</Text>
+        </View>
+        <Button title="LogOut" onPress={() => logOut()} />
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Profile;
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    padding: 10,
+  },
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginLeft: 115,
+  },
+});
